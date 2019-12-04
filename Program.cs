876dd9bbe349908fc36caafa58fc873623afae07
SukaSharagaZaebala <tsibinegor@yandex.ru>
2019-12-04 13:06:56 +0300

    Update Program.cs
    v2

--- Program.cs
+++ Program.cs
@@ -1,21 +1,71 @@
 ﻿using System;
 
-namespace Lab1
+using System.Collections.Generic;
+
+namespace Lab2
 {
     class A
     {
+        protected float x;
 
+        public float solveLin(float a, float b)
+        {
+            if (a == 0.0f || b == 0.0f)
+            {
+                return float.NaN;
+            }
+
+            x = -b / a;
+            return x;
+        }
     }
 
     class B : A
     {
-        private A a;
-        private A b;
+        private float x2;
 
-        public B(A a, A b)
+        public List<float> solveQuad(float a, float b, float c)
         {
-            this.a = a;
-            this.b = b;
+            if (a == 0.0f)
+            {
+                float result = solveLin(b, c);
+
+                if (float.IsNaN(result))
+                {
+                    return new List<float>();
+                }
+                else
+                {
+                    x = result;
+                    return new List<float>{ result };
+                }
+            }
+
+            float disc = calcDisc(a, b, c);
+
+            if (disc < 0)
+            {
+                return new List<float>();
+            }
+
+            List<float> roots = new List<float>();
+            roots.Add((-b + (float)Math.Sqrt(disc)) / 2 * a);
+            x = roots[0];
+
+            if (disc == 0.0f)
+            {
+                return roots;
+            }
+
+            roots.Add((-b - (float)Math.Sqrt(disc)) / 2 * a);
+            x2 = roots[1];
+
+            return roots;
+        }
+
+        protected float calcDisc(float a, float b, float c)
+        {
+            return b * b - 4 * a * c;
         }
     }
 
@@ -23,7 +73,27 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            A a = new B(new A(), new B(new A(), new A()));
+            B slv = new B();
+
+            List<float> roots = slv.solveQuad(0, 0, 0);
+
+            if (roots.Count == 0)
+            {
+                Console.WriteLine("Корней нет");
+            }
+            else
+            {
+                Console.Write("Корни: ");
+
+                foreach (float root in roots)
+                {
+                    Console.Write(root.ToString() + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
