e84abdae9b4d6f0e9c8b0fdd6c035391a4753588
SukaSharagaZaebala <tsibinegor@yandex.ru>
2019-12-04 13:05:59 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab1
+{
+    class A
+    {
+
+    }
+
+    class B : A
+    {
+        private A a;
+        private A b;
+
+        public B(A a, A b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new B(new A(), new B(new A(), new A()));
+        }
+    }
+}
